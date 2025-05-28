3f5d4eaed196d7c9bafd71ea31063c711f373626
Ima-server <imaosglobal@gmail.com>
2025-05-28 10:28:03 +0300

    server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express();
-const port = process.env.PORT || 3000;
+const port = (process && process.env && process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
